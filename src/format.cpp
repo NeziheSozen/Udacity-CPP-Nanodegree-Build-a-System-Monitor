63b1de6c970f4f38b05eecd866675ab60a22f12d
Nezihe Sözen <nezihe.sozen@gmail.com>
2023-04-02 23:15:35 +0300

    formnat.cpp has been implemented

--- src/format.cpp
+++ src/format.cpp
@@ -4,8 +4,32 @@
 
 using std::string;
 
-// TODO: Complete this helper function
+void convertUnit(std::string &input, long unit)
+{
+  if (unit < 10){
+    input = "0" + std::to_string(unit);
+  }
+  else {
+    input = std::to_string(unit);
+  }
+}
+
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds)
+{ 
+  long second = seconds % 60;
+  long minute = (seconds / 60) % 60;
+  long hour = (seconds / (60 * 60)) % 24;
+
+  std::string ss = "";
+  std::string mm = "";
+  std::string hh = "";
+
+  convertUnit(ss, second);
+  convertUnit(mm, minute);
+  convertUnit(hh, hour);
+
+  return string(hh + ":" + mm + ":" + ss);
+
+}
